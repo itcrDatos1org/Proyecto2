939c896f5839c285242f355619239a1adeae6600
LumberMan1301 <marianomm@rocketmail.com>
2017-10-19 23:43:23 -0600

    Controlador de la interfaz

---
+++ EcliSer/src/Interfaz/Interfaz.java
@@ -0,0 +1,16 @@
+package Interfaz;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class Interfaz implements Initializable{
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
